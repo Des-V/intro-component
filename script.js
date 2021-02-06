7eb0cfa6cb6d8ab4008eef5e235f61dd15dc221c
Desiree-Valdes <desireevaldes@gmail.com>
2021-02-06 16:26:00 +0100

    [004] First mobile design portrait
    [004] First mobile design portrait

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // Accessing the DOM and assign it to variables
 const headerTitle = document.getElementById("headerTitle");
 const headerText = document.getElementById("headerText");
-//const promotionTextBold = document.getElementById("promotionTextBold");
 const promotionText = document.getElementById("promotionText");
 const buttonClaim = document.getElementById("buttonClaim");
 const smallText = document.getElementById("smallText");
-//const smallTextLink = document.getElementById("smallTextLink");
+const smallTextLink = document.getElementById("smallTextLink");
+const form = document.getElementById("form");
 const formList = document.getElementById("formList");
+const fname = document.getElementById("name");
+const surname = document.getElementById("surname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 const formTerms = document.getElementById("formTerms");
 const backgroundWrapper = document.getElementById("wrapper");
 const body = document.getElementById("body");
@@ -14,11 +18,9 @@ const body = document.getElementById("body");
 // Adding texts to the HTML Structure
 headerTitle.innerHTML = 'Learn to code by watching others';
 headerText.innerHTML = 'See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.';
-//promotionTextBold.innerHTML = 'Try it free 7 days';
-promotionText.innerHTML = 'Try it free 7 days then $20/mo. thereafter';
+promotionText.innerHTML = '<b>Try it free 7 days</b> then $20/mo. thereafter';
 buttonClaim.innerHTML = 'CLAIM YOUR FREE TRIAL';
-smallText.innerHTML = 'By clicking the button you are agreeing to our Terms and Services';
-//smallTextLink.innerHTML = 'Terms and Services';
+smallText.innerHTML = 'By clicking the button you are agreeing to our <a id="smallTextLink" href="#" alt="Terms and Services" style="color: rgb(255, 120, 124)">Terms and Services</>';
 
 // Styling body HTML element body
 body.style.backgroundImage = "url(img/bg-intro-desktop.png)";
@@ -28,41 +30,63 @@ body.style.backgroundAttachment = 'fixed';
 body.style.fontFamily = "'Poppins', sans-serif";
 
 // Styling wrapper HTML element div
-backgroundWrapper.style.display = 'grid';
-backgroundWrapper.style.textAlign = 'center'; 
-backgroundWrapper.style.backgroundColor = "rgb(255, 121, 120)"; // Orange bg color
-backgroundWrapper.style.opacity = '3';
+backgroundWrapper.style.backgroundColor = "rgba(255, 121, 120, 0.7)"; // Orange bg color
+
 
 // Styling headerTitle HTML element h1
 headerTitle.style.color = 'white';
+headerTitle.style.fontSize = '22px';
 
 // Styling headerText HTML element p
 headerText.style.color = 'white';
-
-// Styling promotionTextBold HTML element p span
-//promotionTextBold.style.fontWeight = 'bold';
-
+headerText.style.fontSize = '13px';
 
 // Styling promotionText HTML element p
 promotionText.style.color = 'white';
 promotionText.style.backgroundColor = 'rgb(93, 84, 163)';
-promotionText.style.borderBottomColor = 'rgb(218, 105, 107)';
+promotionText.style.boxShadow = '0px 10px rgb(218, 105, 107)';
+promotionText.style.borderRadius = '10px';
+promotionText.style.fontSize = '14px';
+
+// Styling form HTML element form
+form.style.fontFamily = "'Poppins', sans-serif";
 
 // Styling formList HTML element ul
 formList.style.backgroundColor = 'white';
-formList.style.borderBottomColor = 'rgb(218, 105, 107)';
+formList.style.boxShadow = '0px 10px rgb(218, 105, 107)';
+formList.style.borderRadius = '10px';
 formList.style.listStyleType = 'none';
+
+// Styling name HTML element form ul li 
+fname.style.fontFamily = "'Poppins', sans-serif";
+fname.style.fontWeight = "bold";
+
+// Styling surname HTML element form ul li 
+surname.style.fontFamily = "'Poppins', sans-serif";
+surname.style.fontWeight = "bold";
+
+// Styling email HTML element form ul li 
+email.style.fontFamily = "'Poppins', sans-serif";
+email.style.fontWeight = "bold";
+
+// Styling password HTML element form ul li 
+password.style.fontFamily = "'Poppins', sans-serif";
+password.style.fontWeight = "bold";
 
 // Styling formTerms HTML element li
 formTerms.style.backgroundColor = 'white';
+formTerms.style.fontFamily = "'Poppins', sans-serif";
+formTerms.style.fontSize = '10px';
 
 // Styling buttonClaim HTML element li
 buttonClaim.style.color = 'white';
 buttonClaim.style.backgroundColor = 'rgb(119, 212, 180)';
-buttonClaim.style.borderBottomColor = 'rgb(137, 212, 180)';
+buttonClaim.style.boxShadow = '0px 5px rgb(141, 189, 167)';
+buttonClaim.style.borderRadius = '10px';
+buttonClaim.style.border = 'none';
+buttonClaim.style.fontWeight = "bold";
 
 // Styling smallText HTML element p
+smallText.style.color = 'gray';
 smallText.style.backgroundColor = 'white'; 
 
-// Styling smallTextLink HTML element a
-//smallTextLink.style.color = 'rgb(255, 120, 124)'; // Orange form
